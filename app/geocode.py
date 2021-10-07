f723b006ea50dd23aca13570bfc2c3686d3c3e5d
BrokenShell <webmaster@sharpdesigndigital.com>
2021-10-07 07:47:30 -0700

    fixes a problem with the geo locator

--- app/geocode.py
+++ app/geocode.py
@@ -14,6 +14,8 @@ async def get_latitude_longitude(address_input: dict):
     The output will be a JSON object with the following format:
 {"latitude": 47.64971, "longitude": -117.39764}
     """
+    if not address_input:
+        return {"latitude": 47.64971, "longitude": -117.39764}
     locator = Nominatim(user_agent="DS API - Family Promise")
     address_string = " ".join(val for val in address_input.values() if val)
     if type(locator.geocode(address_string)) == geopy.location.Location:
@@ -29,4 +31,4 @@ async def get_latitude_longitude(address_input: dict):
                 "longitude": locator.geocode(address_string).longitude,
             }
         else:
-            return {"Result": "Error"}
+            return {"Result": "Error, location not found"}
