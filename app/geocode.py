995c5507d267f6ac9ac593b4cacfac0a18bd390f
BrokenShell <webmaster@sharpdesigndigital.com>
2021-10-07 07:37:49 -0700

    fixes a problem with the geo locator

--- app/geocode.py
+++ app/geocode.py
@@ -9,18 +9,24 @@ router = APIRouter()
 async def get_latitude_longitude(address_input: dict):
     """
     Please post an address as a JSON object with this format:
-
 {"address": "123 Gilman Dr W", "address_line2": "", "city": "Seattle", "state": "WA", "zip": "98119", "country": "United States"}
 
     The output will be a JSON object with the following format:
 {"latitude": 47.64971, "longitude": -117.39764}
-
-    Default values point to a the Family Promise Homeless Shelter
     """
-    lat_long = {"latitude": 47.649710, "longitude": -117.397640}
-    locator = Nominatim(user_agent='DS API - Family Promise')
+    locator = Nominatim(user_agent="DS API - Family Promise")
     address_string = " ".join(val for val in address_input.values() if val)
     if type(locator.geocode(address_string)) == geopy.location.Location:
-        lat_long["latitude"] = locator.geocode(address_string).latitude
-        lat_long["longitude"] = locator.geocode(address_string).longitude
-    return lat_long
+        return {
+            "latitude": locator.geocode(address_string).latitude,
+            "longitude": locator.geocode(address_string).longitude,
+        }
+    else:
+        address_string = " ".join(val for key, val in address_input.items() if key != "address_line2")
+        if type(locator.geocode(address_string)) == geopy.location.Location:
+            return {
+                "latitude": locator.geocode(address_string).latitude,
+                "longitude": locator.geocode(address_string).longitude,
+            }
+        else:
+            return {"Result": "Error"}
